cb3bf1b1efbb20726956b982d7de34f5c45d44b8
yelmouss <yassine.elmouss@sfr.fr>
2023-02-21 15:56:46 +0100

    Home Card map

--- src/componnents/pages/Home.jsx
+++ src/componnents/pages/Home.jsx
@@ -3,93 +3,50 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-import {BsFillSuitHeartFill,BsSuitHeart,BsEye }  from "react-icons/bs";
+import { ProjectsData } from "../../data/tableaux";
+import Font, { Text } from "react-font";
+import { BsFillSuitHeartFill, BsSuitHeart, BsEye } from "react-icons/bs";
 import { TfiShoppingCartFull } from "react-icons/tfi";
-const Home = () => {
+const Home =  ({ dark, updateDark }) => {
   return (
     <Container
-      className=" d-flex flex-column min-vh-100 SVGGround bg-light"
+    className={`d-flex flex-column min-vh-100 SVGGround ${dark ? "bg-light text-dark" : "bg-dark text-light"}`} 
       fluid
     >
-      <Container className="mt-2">
-        <Row xs="auto" lg={4} md={3}>
-          <Col>
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://cdn.shopify.com/s/files/1/0068/0283/3463/files/tableau-decoratif-design-massinart_350x.jpg?v=1644578629"
-              />
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <Card.Body className="fs-5 text-dark">
-                <Card.Link href="#"><TfiShoppingCartFull /></Card.Link>
-                <Card.Link href="#"><BsSuitHeart /></Card.Link>
-                <Card.Link href="#"><BsEye /></Card.Link>                
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
+      <Container className="mt-2 p-2">
+        <Font family="Zeyada">
+          <h1 className="fw-bolder  p-2">
             {" "}
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://cdn.shopify.com/s/files/1/0068/0283/3463/files/tableau-decoratif-design-massinart_350x.jpg?v=1644578629"
-              />
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            {" "}
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://cdn.shopify.com/s/files/1/0068/0283/3463/files/tableau-decoratif-design-massinart_350x.jpg?v=1644578629"
-              />
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            {" "}
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://cdn.shopify.com/s/files/1/0068/0283/3463/files/tableau-decoratif-design-massinart_350x.jpg?v=1644578629"
-              />
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-              <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
+            Bienvenue dans notre galerie digitale artistique
+          </h1>
+        </Font>
+        <Row xs="auto" lg={4} md={3} className="p-2">
+          {ProjectsData.map((item, i) => (
+            <Col className="mt-2">
+              <Card className={`${dark ? "" : "text-dark"}`}>
+                <Card.Img
+                  className="MyImgCard"
+                  variant="top"
+                  src={item.image}
+                />
+                <Card.Body>
+                  <Card.Text>{item.description}</Card.Text>
+                  <Card.Text>{item.price} $</Card.Text>
+                </Card.Body>
+                <Card.Body className="fs-5 text-dark">
+                  <Card.Link href="#">
+                    <TfiShoppingCartFull />
+                  </Card.Link>
+                  <Card.Link href="#">
+                    <BsSuitHeart />
+                  </Card.Link>
+                  <Card.Link href="#">
+                    <BsEye />
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
---
+++ src/data/tableaux.js
@@ -0,0 +1,52 @@
+export const ProjectsData = [
+
+    {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0068/0283/3463/files/tableau-decoratif-design-massinart_350x.jpg?v=1644578629",
+    price: 10,
+  },
+  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0068/0283/3463/products/tableau-line-art-faces-gold-silver-decoration-artistique-moderne-abstrait-dore-argente-visages-massinart-faces-noir.jpg?v=1656191948",
+    price: 10,
+  },
+  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0068/0283/3463/products/tableaumoderne_vertetdore_marbleeffet_decorationinterieure_salonmarocain_nature_ile_54d10d28-674a-42be-b3a5-1fa689d5125f.jpg?v=1624821918",
+    price: 10,
+  },
+  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },  {
+    title: "Lorem Ipsum",
+    description: `tableau decoratif design massinart`,
+    image: "https://cdn.shopify.com/s/files/1/0224/7874/9770/products/Tableau-moderne-art-Abstrait---SPTABST007.jpg?v=1566398357",
+    price: 10,
+  },
+]
