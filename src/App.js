3bfce8199f6cbe0ef1f3706ba13d8ecdce2633e2
yelmouss <yassine.elmouss@sfr.fr>
2023-02-21 12:28:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+import { Button } from "react-bootstrap";
+import { FaBeer } from 'react-icons/fa';
 function App() {
   return (
     <div className="App">
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button as="a" variant="success">
+          Button as link <FaBeer />
+        </Button>
       </header>
     </div>
   );
