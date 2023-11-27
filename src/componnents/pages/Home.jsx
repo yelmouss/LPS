24ad331fc6edaec02f3ac61caef835d956d89c2b
yelmouss <yelmouss.devt@gmail.com>
2023-11-27 12:36:29 +0100

    first commit

--- src/componnents/pages/Home.jsx
+++ src/componnents/pages/Home.jsx
@@ -87,10 +87,10 @@ const Home = ({ dark, updateDark }) => {
     <><Carousel data-bs-theme="dark" className="sticky">
       <Carousel.Item>
         <img
-          className="d-block w-100 sliderimg overlay"
+          className="d-block w-100 sliderimg"
           src="https://actinutrition.fr/wp-content/uploads/2020/01/legume-hiver-1000.jpg"
           alt="First slide" />
-        <Carousel.Caption>
+        <Carousel.Caption className="bg-dark bg-opacity-50" >
           <h5>Fruits & légumes</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
@@ -100,7 +100,7 @@ const Home = ({ dark, updateDark }) => {
           className="d-block w-100 sliderimg"
           src="./fondation.jpg"
           alt="Second slide" />
-        <Carousel.Caption>
+        <Carousel.Caption className="bg-dark bg-opacity-50">
           <h5>Produits Alimentaires</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
@@ -110,7 +110,7 @@ const Home = ({ dark, updateDark }) => {
           className="d-block w-100 sliderimg"
           src="https://www.menage-services-perigueux.com/wp-content/uploads/menage-jardinage2-1.jpg"
           alt="Third slide" />
-        <Carousel.Caption>
+        <Carousel.Caption className="bg-dark bg-opacity-50">
           <h5>Bricolage et jardinage</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
