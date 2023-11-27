cfd41b2b9d5f4e6dbbd9f14bfda440eed8432d27
yelmouss <yelmouss.devt@gmail.com>
2023-11-27 17:44:44 +0100

    first commit

--- src/componnents/pages/Slila.jsx
+++ src/componnents/pages/Slila.jsx
@@ -4,8 +4,8 @@ import Maticha from  '../../citrons-bio.webp'
 
 function Slila(props) {
   return (
-    <div className='Slila'>
-        <img src={LaSlilafrom} alt="" className='img-fluid lgouffa' />
+    <div className='Slila mt-5'>
+        <img src={LaSlilafrom} alt="" className='img-fluid lgouffa mt-5' />
         <img src={props.t9edia} alt="" className='img t9edia' />
 
        {props.comande}
